4ff59a240b020edde6082afb6bc2d692c5d69c0c
Amir <amir_tarig@yahoo.com>
2020-09-26 13:17:17 +0200

    dropdown

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+ <header>
+   <Navbar/>
+ </header>
+</template>
+
+<script>
+import Navbar from './components/NavBar'
+
+
+export default {
+  name: 'App',
+  components: {
+   Navbar,
+  }
+}
+</script>
+
+<style>
+
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+body{
+  font-family: 'montserrat',sans-serif;
+
+}
+
+header{
+  width:100vw;
+  background-color: #222;
+  padding: 15px;
+}
+</style>
